1b4304ded8f885ddbe036559980d4a11a06de3ee
Angel Ricardo <angelricardo.dev@gmail.com>
2020-10-29 23:57:00 -0600

    Deleting localstack profile

--- aws/env-setup.sh
+++ aws/env-setup.sh
@@ -3,20 +3,17 @@
 # CREATE S3 BUCKET TO STORE NODE PACKAGED CODE (.zip)
 aws s3api create-bucket \
   --bucket javascript.code.itl \
-  --endpoint-url=http://localhost:4566 \
-  --profile localstack
+  --endpoint-url=http://localhost:4566
 
 # UPLOAD CODE
 aws s3api put-object \
   --bucket javascript.code.itl \
   --key application-code.zip \
   --body built.zip \
-  --endpoint-url=http://localhost:4566 \
-  --profile localstack
+  --endpoint-url=http://localhost:4566
 
 # CREATES CloudFormation: stack includes dynamodb databases and lambda functions 
 aws cloudformation create-stack \
   --endpoint-url=http://localhost:4566 \
   --stack-name sample-stack \
-  --template-body file://aws/cloudformation.yaml \
-  --profile localstack
+  --template-body file://aws/cloudformation.yaml
--- aws/update-lambda-code.sh
+++ aws/update-lambda-code.sh
@@ -4,5 +4,4 @@
 aws lambda update-function-code \
     --function-name create-user \
     --zip-file fileb://built.zip \
-    --endpoint-url=http://localhost:4566 \
-    --profile localstack
+    --endpoint-url=http://localhost:4566
